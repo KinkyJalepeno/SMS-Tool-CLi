9d9ef91dadc8c71967f9881bb59556a4bdba3e00
KinkyJalepeno <kinkyjalepeno@gmail.com>
2017-11-27 15:13:58 +0000

    Added working menu and method stubs

--- src/smsender/SMSender.java
+++ src/smsender/SMSender.java
@@ -3,7 +3,6 @@ import java.io.*;
 import java.net.Socket;
 import java.util.*;
 
-
 /**
  *
  * @author Dave.Graham
@@ -32,17 +31,81 @@ public class SMSender {
         //send authentication string including IP and password
         p.println("{\"method\":\"authentication\",\"server_password\":\"" + pass + "\"}");        
             String response = sc1.nextLine();
-            System.out.println(response.substring(32, 45) + "\n");
+            System.out.println("\n" + response.substring(32, 45) + "\n");
             
         p.println("");
             response = sc1.nextLine();
-            System.out.println(response);
+            System.out.println("\n" + response);
             
-       
+            Thread.sleep(1000);
+        
+        while(true) {
+            p.println("{\"method\":\"authentication\",\"server_password\":\"" + pass + "\"}");
+            System.out.println("\t Dave's SMS Sender Application");
+            System.out.println("\t -----------------------------");
+            System.out.println("\t      Select an Option");
+            System.out.println("\t -----------------------------");
+            System.out.println("0 \t Exit");
+            System.out.println("1 \t Send an SMS");
+            System.out.println("2 \t Send a multi-port SMS");
+            System.out.println("3 \t Reserved");
+            System.out.println("4 \t Reserved");
+            System.out.print("Option: ");
+            
+            // start scanner to receive option
+            int option = sc.nextInt();
+
+            // break out of infinite loop on selecting 0
+            if (option == 0) {
+                System.out.println("You chose option " + option + " to exit the program \n");
+                break;
+            }// end if for break
+            
+        // Start switch to check option and call matching method
+            switch (option) {
+
+                case 1:
+                    sendSingle();
+                    break;
+
+                case 2:
+                    sendMulti();
+                    break;
+
+                case 3:
+                    reserve();
+                    break;
+
+                case 4:
+                    reserve2();
+                    break;
+
+                default:
+                    System.err.println("You must select an option from 0 to 4 ! \n");
+                    break;
+            } // end switch
+        }// end infinite loop
         sc.close();
         s.close();
-        
-        
-    }//end main
-   
+     }//end main
+    
+    private static void sendSingle(){
+      System.out.println("You chose to send a message");
+      //return;
+    }
+    
+    private static void sendMulti(){
+      System.out.println("You chose to send a multi-message");
+      //return;  
+    }
+    
+    private static void reserve(){
+      System.out.println("This option is reserved for later");
+      //return;  
+    }
+    
+    private static void reserve2(){
+      System.out.println("This option is reserved for later");
+      //return;  
+    }
 }//end class
